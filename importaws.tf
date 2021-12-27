de803410e0dc18538142de25539c9832c5fc0b5f
KUMAR Pravin <g509569@mph.morpho.com>
2021-12-27 18:02:42 +0530

    yes

--- importaws.tf
+++ importaws.tf
@@ -1,8 +1,6 @@
 resource "aws_instance" "myecc1" {
     ami           = "ami-0e21d4d9303512b8e"
     instance_type = "t2.micro"
-    vpc_security_group_ids=["sg-0aad482c56d805b5d"]
-    subnet_id = "subnet-03e0a62bed3091c70"
-   
+      
   
 }
